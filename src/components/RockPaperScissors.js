6d72519eb45bcb5ffe1ce4736cf33c886252328e
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-08 10:46:12 -0500

    Adds game mechanics

--- src/components/RockPaperScissors.js
+++ src/components/RockPaperScissors.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
-import useFetch from "../hooks/useFetch";
 
 export default function RockPaperScissors({ experienceUp, level }) {
-  const [playerGuess, setPlayerGuess] = useState("rock");
+  const [playerGuess, setPlayerGuess] = useState(null);
   const [playerImage, setPlayerImage] = useState();
   const [computerGuess, setComputerGuess] = useState(null);
   const [computerImage, setComputerImage] = useState();
   const [result, setResult] = useState(null);
+
+  let options = ["rock", "paper", "scissors"];
+
+  useEffect(() => {
+    if (playerGuess) {
+      setComputerGuess(options[Math.floor(Math.random() * 3)]);
+    }
+  }, [playerGuess]);
 
   return (
     <div>
@@ -15,19 +22,44 @@ export default function RockPaperScissors({ experienceUp, level }) {
       {/* Game Board */}
       <div className="game-board margin-center flex space-between border">
         <div className="rps-player">
-          {playerGuess && <div>You chose {playerGuess}</div>}
+          {result && <div>You chose {playerGuess}
+          <div className="choice"></div></div>}
         </div>
-
+        <button
+          onClick={() => {
+            if (!result) {
+              if (playerGuess === computerGuess) {
+                setResult("draw");
+              } else if (
+                (playerGuess === "rock" && computerGuess === "scissors") ||
+                (playerGuess === "scissors" && computerGuess === "paper") ||
+                (playerGuess === "paper" && computerGuess === "rock")
+              ) {
+                setResult("win");
+                if (level < 2) {
+                  experienceUp(10);
+                }
+              } else {
+                setResult("loss");
+              }
+            }
+          }}
+        >
+          Shoot
+        </button>
         <div className="rps-computer">
-          {computerGuess && <div>Computer chose {computerGuess}</div>}
+          {result && <div>Computer chose {computerGuess}
+          <div className="choice"></div></div>}
         </div>
       </div>
+      <div>{result}</div>
       {/* Player Choices */}
       <div className="margin-center flex game-options">
         <button
           className="margin-center option-button"
           onClick={() => {
             setPlayerGuess("rock");
+            setResult(null);
           }}
         >
           Rock
@@ -36,6 +68,7 @@ export default function RockPaperScissors({ experienceUp, level }) {
           className="margin-center option-button"
           onClick={() => {
             setPlayerGuess("paper");
+            setResult(null);
           }}
         >
           Paper
@@ -44,6 +77,7 @@ export default function RockPaperScissors({ experienceUp, level }) {
           className="margin-center option-button"
           onClick={() => {
             setPlayerGuess("scissors");
+            setResult(null);
           }}
         >
           Scissors
