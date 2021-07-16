9f61d8e2c554e83b1a5bc2f2deb3d809774a8667
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-16 16:04:51 -0500

    Creates signup on routes file

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const { signup } = require("../models/users.model");
+
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  if (
+    username &&
+    password &&
+    username.length > 2 &&
+    username.length <= 20 &&
+    password.length > 2 &&
+    password.length <= 20
+  ) {
+      return signup(res, username, password);
+  }
+  return res.send({success: false,
+    data: null,
+    error: "Invalid data provided"});
+});
 
 module.exports = router;
