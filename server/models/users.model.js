c8b5e01777730c0730f409a028db518ffd9d74f0
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-16 15:58:51 -0500

    Adds Signup to models

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,5 +1,34 @@
+const { response } = require("express");
 const query = require("../config/mysql.conf");
 
+async function signup()
+{
+    let json = {success: false, data: null, error: null};
+    try {
+        const users = await query("SELECT * FROM users WHERE username = ?", [username]);
+        if (users.length !==0)
+        {
+            json.error = "Username not available. Please choose another";
+        } else {
+            const hashed = await bcrypt.hash(password, 10);
+            await query("INSERT INTO users (password, username) VALUES (?,?)", [hashed, username]);
+            json = { ...json, success: true };
+        }
+    } catch (err) {
+        console.log(err);
+        json.error = "Signup failed"
+    } finally {
+        return response.send(json);
+    }
+}
 
+async function login()
+{}
 
-module.exports = { }
+async function changePassword()
+{}
+
+async function deleteAccount()
+{}
+
+module.exports = { signup }
