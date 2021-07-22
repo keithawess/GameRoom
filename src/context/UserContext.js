765d9a5a3378683639a7a87c05175b380613b3fa
keithawess <61606760+keithwess@users.noreply.github.com>
2021-07-22 12:16:42 -0500

    Creates logout and validate useEffect

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -21,4 +21,34 @@ export function UserProvider(props){
         setUserId(user.id);
     },[]);
 
+    const logout = useCallback (async () => {
+        const res = await logoutCall("/api/users/logout");
+        if(res.success) {
+            setUserId("");
+            setUsername("");
+            setLevel(0);
+            setExperience(0);
+            setBuddy(null);
+            setNavScroll(0);
+        }
+    }, []);
+
+    useEffect(() => {
+        async function validate() {
+          const res = await validateCall("/api/users/validate");
+          if (res.success) {
+            login(res.data);
+    
+            let buddyRes = await buddyCall(
+              `/api/buddies/user/${res.data.id}`
+            );
+            setUsername(res.data.username);
+            if (buddyRes.error) {
+              return;
+            }
+            setBuddy(buddyRes.data);
+          }
+        }
+        validate();
+      }, []);
 }
